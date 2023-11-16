9c2818d2e966b96b93b282e513d8cfb361190deb
oliverbill <alves.bill@gmail.com>
2023-11-16 22:48:39 +0000

    add static files for django-admin

--- homeinventoryapp/settings.py
+++ homeinventoryapp/settings.py
@@ -162,6 +162,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = './static-djangoadmin'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
