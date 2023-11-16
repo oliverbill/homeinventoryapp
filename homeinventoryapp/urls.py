67a88e6ad592cc9a14ab9c71bb9ef1241b895934
oliverbill <alves.bill@gmail.com>
2023-11-16 23:46:07 +0000

    add static url path

--- homeinventoryapp/urls.py
+++ homeinventoryapp/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
+from homeinventoryapp import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("homeinventoryapi.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
